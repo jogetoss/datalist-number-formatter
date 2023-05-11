a1a2484fb69051ceed750867aa1518009bc8b301
Hugo Lim <hugo@joget.org>
2023-05-12 07:08:20 +0800

    Modified: Support lookup of value from other columns for value, prefix, and postfix by using {columnName}

--- src/main/java/org/joget/marketplace/DatalistNumberFormatter.java
+++ src/main/java/org/joget/marketplace/DatalistNumberFormatter.java
@@ -1,11 +1,16 @@
 package org.joget.marketplace;
 
+import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import org.joget.apps.app.service.AppPluginUtil;
 import org.joget.apps.app.service.AppUtil;
 import org.joget.apps.datalist.model.DataList;
 import org.joget.apps.datalist.model.DataListColumn;
 import org.joget.apps.datalist.model.DataListColumnFormatDefault;
+import org.joget.apps.datalist.service.DataListService;
 import org.joget.commons.util.StringUtil;
+import org.joget.commons.util.TimeZoneUtil;
 
 public class DatalistNumberFormatter extends DataListColumnFormatDefault{
     private final static String MESSAGE_PATH = "messages/DatalistNumberFormatter";
@@ -17,7 +22,7 @@ public class DatalistNumberFormatter extends DataListColumnFormatDefault{
 
     @Override
     public String getVersion() {
-        return "8.0.0";
+        return "8.0.1";
     }
 
     @Override
@@ -39,7 +44,7 @@ public class DatalistNumberFormatter extends DataListColumnFormatDefault{
     public String format(DataList dataList, DataListColumn column, Object row, Object value) {
         String num = (String) value;
         String result = "";
-        result = StringUtil.numberFormat(num, getPropertyString("style"), getPropertyString("prefix"), getPropertyString("postfix"), "true".equalsIgnoreCase(getPropertyString("useThousandSeparator")), getPropertyString("numOfDecimal"));
+        result = StringUtil.numberFormat(getValueFromRow(num, row), getPropertyString("style"), getValueFromRow(getPropertyString("prefix"), row), getValueFromRow(getPropertyString("postfix"), row), "true".equalsIgnoreCase(getPropertyString("useThousandSeparator")), getPropertyString("numOfDecimal"));
 
         return result;
    }
@@ -47,5 +52,41 @@ public class DatalistNumberFormatter extends DataListColumnFormatDefault{
     @Override
     public String getPropertyOptions() {
         return AppUtil.readPluginResource(getClass().getName(), "/properties/DatalistNumberFormatter.json", null, true, MESSAGE_PATH);    
+    }
+    
+    protected String getValueFromRow(String expr, Object row) {
+        Pattern pattern = Pattern.compile("\\{([a-zA-Z0-9_]+)\\}");
+        Matcher matcher = pattern.matcher(expr);
+        
+        while (matcher.find()) {
+            String key = matcher.group(1);
+            Object value = evaluate(row, key);
+            if (value != null) {
+                expr = expr.replaceAll(StringUtil.escapeRegex("{"+key+"}"), value.toString());
+            }
+            //try again with c_
+            if(value == null && !key.startsWith("c_")){
+                value = evaluate(row, "c_" + key);
+                if (value != null) {
+                    expr = expr.replaceAll(StringUtil.escapeRegex("{"+key+"}"), value.toString());
+                }
+            }
+        }
+        
+        return expr;
+    }
+    
+    protected Object evaluate(Object row, String propertyName) {
+        if (propertyName != null && !propertyName.isEmpty()) {
+            try {
+                Object value = DataListService.evaluateColumnValueFromRow(row, propertyName); 
+                //handle for lowercase propertyName
+                if (value == null) {
+                    value = DataListService.evaluateColumnValueFromRow(row, propertyName.toLowerCase());
+                }
+                return value;
+            } catch (Exception e) {}
+        }
+        return null;
     }
 }
